6bc8d295127aa2e841b6da99761e5065c3a8f638
Muhammad Usman <prousman741@gmail.com>
2022-01-15 15:20:07 +0500

    Navbar implemented

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { ReactComponent as Logo } from '../../assets/images/logo.svg'
+import { ReactComponent as Sun } from '../../assets/images/sun.svg'
+import { ReactComponent as Moon } from '../../assets/images/moon.svg'
 
 const Navbar = () => {
     return (
-        <div>
-            <p>This is Navbar</p>
-        </div>
+        <ul className='navbar__container'>
+            <Link to='/' className='navbar__item'>
+                <Logo className='navbar__item-logo' />
+            </Link>
+            <li className='navbar__item'>
+                <ul className='navbar__item-page'>
+                    <Link to='/artist' className='navbar__item navbar-link'>Artist</Link>
+                    <Link to='/event' className='navbar__item navbar-link'>Events</Link>
+                    <li className='navbar__item'>
+                        <Moon />
+                    </li>
+                </ul>
+            </li>
+        </ul>
     )
 }
 
