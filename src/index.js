fe7333b0ca31614b12c0d734f539f94679e6bec3
Muhammad Usman <prousman741@gmail.com>
2022-01-15 14:28:46 +0500

    project initial folders structure & files made

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './scss/_main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
