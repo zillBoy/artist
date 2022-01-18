0bb568eae84ccf937ddd9d9f619786cbf956f5cc
Muhammad Usman <prousman741@gmail.com>
2022-01-18 22:14:00 +0500

    HeaderEvents - headings added

--- src/components/Header/HeaderEvents.jsx
+++ src/components/Header/HeaderEvents.jsx
@@ -5,7 +5,16 @@ import { artists } from '../../utils/Utils'
 const HeaderEvents = () => {
     return (
         <div className='headerevents__container'>
-            {artists.map((artist, index) => <HeaderArtistCard key={index} image={artist.image} name={artist.name} />)}
+            <div className='headerevents__container-main'>
+                <h1 className='heading-primary headerevents__container-main--heading'>Top Events</h1>
+                <div className='headerevents-textcontent'>
+                    <h1 className='heading-event'>Artist</h1>
+                    <h1 className='heading-event heading-event--subheading'>Events</h1>
+                </div>
+            </div>
+            <div className='headerevents__container-artists'>
+                {artists.map((artist, index) => <HeaderArtistCard key={index} image={artist.image} name={artist.name} />)}
+            </div>
         </div>
     )
 }
