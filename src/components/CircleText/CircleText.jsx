56967384ec9fae8a5f0e12513e189d09ae86d05a
Muhammad Usman <prousman741@gmail.com>
2022-01-19 12:00:48 +0500

    CircleText - added

---
+++ src/components/CircleText/CircleText.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Text } from '../../assets/images/textimg.svg'
+
+const CircleText = () => {
+  return (
+    <div className='circletext__container'>
+        <Text className='circletext__container-icon' />
+    </div>
+  );
+};
+
+export default CircleText;
--- src/components/Header/HeaderImage.jsx
+++ src/components/Header/HeaderImage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ProductLandingPage from '../../assets/images/productlandingpage.png'
+import CircleText from '../CircleText/CircleText'
 
 const HeaderImage = () => {
     return (
-        <div className='header__imagediv'>
-            <img className='header__imagediv-image' src={ProductLandingPage} alt='product landing page' />
-        </div>
+        <>
+            <CircleText />
+            <div className='header__imagediv'>
+                <img className='header__imagediv-image' src={ProductLandingPage} alt='product landing page' />
+            </div>
+        </>
     )
 }
 
